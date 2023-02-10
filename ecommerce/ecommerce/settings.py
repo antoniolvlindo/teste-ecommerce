eebd2b4b23b9526dcea110ce8504af60ee100b65
Antonio Lindo <antoniolvitall@gmail.com>
2023-02-09 22:49:01 -0300

    criando modelos em models.py

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'store',  # app da loja
 ]
 
 MIDDLEWARE = [
--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=250, db_index=True)
+    slug = models.SlugField(max_length=250, unique=True)
+
+    class Meta:
+        verbose_name_plural = 'categorias'
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=250)
+    brand = models.CharField(max_length=250, default='sem-marca')
+    description = models.TextField(blank=True)
+    slug = models.SlugField(max_length=255)
+    price = models.DecimalField(max_digits=4, decimal_places=2)
+    # image = models.ImageField(upload_to='images/')
+
+    class Meta:
+        verbose_name_plural = 'produtos'
+
+    def __str__(self):
+        return self.title
