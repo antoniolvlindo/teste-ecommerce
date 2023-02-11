f5f051114737684bd058680a3632a7fb96bb5d0d
Antonio Lindo <antoniolvitall@gmail.com>
2023-02-11 10:47:39 -0300

    mudanças no django admin e criação do arquivo de urls

--- ecommerce/store/admin.py
+++ ecommerce/store/admin.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 
 from .models import Category, Product
 
-admin.site.register(Category)
 
-admin.site.register(Product)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
