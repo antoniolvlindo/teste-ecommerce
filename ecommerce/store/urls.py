9a0b86a2ecd0ec1516c4e1d1f07622bc156d8d4c
Antonio Lindo <antoniolvitall@gmail.com>
2023-02-12 11:30:51 -0300

    criando função de filtragem por categoria

--- ecommerce/store/urls.py
+++ ecommerce/store/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.store, name='store'),
-    path('product/<slug:slug>/', views.product_info, name='product-info')
+    path('product/<slug:product_slug>/',
+         views.product_info, name='product-info'),
+    path('search/<slug:category_slug>/',
+         views.list_category, name='product-info'),
 ]
