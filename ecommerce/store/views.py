3044fdc1a79d26863c873a7d1ae309d41b0ebca0
Antonio Lindo <antoniolvitall@gmail.com>
2023-02-11 22:00:30 -0300

    alteração em views para produtos

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 
-from .models import Category
+from .models import Category, Product
 
 
 def store(request):
-    return render(request, 'store/store.html')
+    all_products = Product.objects.all()
+    context = {'all_products': all_products}
+    return render(request, 'store/store.html', context)
 
 
 def categories(request):
