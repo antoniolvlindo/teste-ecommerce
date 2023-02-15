b77af1edcac9dde3a974b08232723a25525b6d05
Antonio Lindo <antoniolvitall@gmail.com>
2023-02-14 21:39:10 -0300

    adicionando os paths em urls do carrinho

--- ecommerce/cart/views.py
+++ ecommerce/cart/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def cart_summary(request):
+    pass
+
+
+def cart_add(request):
+    pass
+
+
+def cart_delete(request):
+    pass
+
+
+def cart_update(request):
+    pass
