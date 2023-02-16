19511f6117008a33c3f34c447977056f3f3e2f27
Antonio Lindo <antoniolvitall@gmail.com>
2023-02-15 23:20:18 -0300

    criando função do carrinho

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -65,6 +65,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'store.views.categories',
+                'cart.context_processors.cart',
             ],
         },
     },
