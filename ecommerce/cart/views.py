2560929c1a730ad0579e8a8eaf58564ecedb8cb8
Antonio Lindo <antoniolvitall@gmail.com>
2023-02-19 19:19:23 -0300

    funcionalidades do carrinho

--- ecommerce/cart/views.py
+++ ecommerce/cart/views.py
@@ -1,3 +1,4 @@
+from django.http import JsonResponse
 from django.shortcuts import get_object_or_404, render
 from store.models import Product
 
@@ -10,13 +11,17 @@ def cart_summary(request):
 
 def cart_add(request):
     cart = Cart(request)
-    if request.Post.get('action') == 'POST':
+    if request.Post.get('action') == 'post':
         product_id = int(request.POST.get('product_id'))
         product_quantity = int(request.POST.get('product_quantity'))
 
         product = get_object_or_404(Product, id=product_id)
 
         cart.add(product=product, product_qty=product_quantity)
+
+        response = JsonResponse({'The product is called: ': product.title,
+                                'and the product quantity is: ': product_quantity})
+        return response
 
 
 def cart_delete(request):
