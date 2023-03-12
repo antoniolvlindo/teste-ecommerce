459e5d84eb3b11c63d653f44571132c10a130b3d
Antonio Lindo <antoniolvitall@gmail.com>
2023-03-12 17:46:40 -0300

    criando funções de registro e arquivo html para a pagina de registro

---
+++ ecommerce/account/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+class CreateUserForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password', 'password2']
+
+    def __init__(self, *args, **kwargs):
+        super(CreateUserForm, self).__init__(*args, **kwargs)
+
+
+# Validação do email
+
+
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError('Esse email é invalido.')
+
+        if len(email >= 350):
+            raise forms.ValidationError("Seu email e muito longo.")
