adf75ed077d3453bee6cc9fc87e6e5486a896c1b
Antonio Lindo <antoniolvitall@gmail.com>
2023-03-13 20:55:10 -0300

    alterando os botoes de registrar para a pagina de registro

--- ecommerce/account/views.py
+++ ecommerce/account/views.py
@@ -1,6 +1,5 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def register(request):
-    return HttpResponse('Account url setup!')
+    return render(request, 'account/registration/register.html')
