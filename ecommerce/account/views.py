0d5da51a72663408ee386a4250c0cbd6dae09c0a
Antonio Lindo <antoniolvitall@gmail.com>
2023-03-17 17:25:51 -0300

    criando urls e views para as páginas de verificação

--- ecommerce/account/views.py
+++ ecommerce/account/views.py
@@ -14,3 +14,19 @@ def register(request):
     context = {'form': form}
 
     return render(request, 'account/registration/register.html', context=context)
+
+
+def email_verification(request):
+    pass
+
+
+def email_verification_sent(request):
+    pass
+
+
+def email_verification_success(request):
+    pass
+
+
+def email_verification_fail(request):
+    pass
