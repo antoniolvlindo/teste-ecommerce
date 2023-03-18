a8c2197b062c913b9c14a389db8098a191e6ee38
Antonio Lindo <antoniolvitall@gmail.com>
2023-03-18 20:55:21 -0300

    arrumando o código

--- ecommerce/account/urls.py
+++ ecommerce/account/urls.py
@@ -6,14 +6,22 @@ urlpatterns = [
     path('register', views.register, name='register'),
 
     # urls de verificacao de email
+
+    # url de verificacao de email
     path('email-verification', views.email_verification, name='email-verification'),
 
+
+    # url de verificacao de email enviada
     path('email-verification-sent', views.email_verification_sent,
          name='email-verification-sent'),
 
+
+    # url de verificacao de email sucesso
     path('email-verification-success', views.email_verification_success,
          name='email-verification-success'),
 
+
+    # url de verificacao de email falhou
     path('email-verification-failed', views.email_verification_fail,
          name='email-verification-failed'),
 
