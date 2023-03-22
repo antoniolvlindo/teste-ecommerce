a32300301c319073220720a7abffa2c9edbfad2f
Antonio Lindo <antoniolvitall@gmail.com>
2023-03-21 22:12:41 -0300

    criando função de verificação de email

--- ecommerce/account/views.py
+++ ecommerce/account/views.py
@@ -16,6 +16,20 @@ def register(request):
             user = form.save()
             user.is_active = False
             user.save()
+
+            # configuracoes de verificacao de email
+
+            current_site = get_current_site(request)
+            subject = 'Account verification email'
+            message = render_to_string('account/registration/email-verification.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': user_tokenizer_generate.make_token(user),
+            })
+
+            user.email_user(subject=subject, message=message)
+
             return redirect('email-verification-sent')
 
     context = {'form': form}
